069003b22fcae8c7843243005d694b5409c27d99
rocbuilder <rocbuilder@gmail.com>
2017-04-11 08:03:39 +0800

    Update millis acc.ino

--- millis acc.ino
+++ millis acc.ino
@@ -1,11 +1,13 @@
-// these constants describe the pins. They won't change:
-//const int groundpin = A1;             // ground
-//const int powerpin = A5;              // voltage
-const int xpin = A0;                  // x-axis of the accelerometer
-const int ypin = A1;                  // y-axis
-const int zpin = A2;                  // z-axis (only on 3-axis models)
 
-float filterVal = 0.2;       // this determines smoothness  - .0001 is max  1 is off (no smoothing)
+
+// these constants describe the pins. They won't change:
+const int groundpin = A0;             // ground
+const int powerpin = A4;              // voltage
+const int xpin = A3;                  // x-axis of the accelerometer
+const int ypin = A2;                  // y-axis
+const int zpin = A1;                  // z-axis (only on 3-axis models)
+
+float filterVal = 0.25;       // this determines smoothness  - .0001 is max  1 is off (no smoothing)
 
 //smoothed value of respective axis
 float sVx = 0; 
@@ -16,7 +18,11 @@ float aV = 0; //acceleration vector
 float aMax = 0;
 
 void setup() {
-  
+  Serial.begin(9600);
+  pinMode(groundpin, OUTPUT);
+  pinMode(powerpin, OUTPUT);
+  digitalWrite(groundpin, LOW);
+  digitalWrite(powerpin, HIGH);
   
 }
 
@@ -30,6 +36,12 @@ void loop() {
   
   aV = sqrt(sq(sVx) + sq(sVy) + sq(sVz)); //acceleration vector
   aV = aMax<0?-aV:aV; //adjust direction of acceleration vector according to aMax
+
+  Serial.print(aMax);
+  Serial.print("\t");
+  Serial.print(aV);
+  Serial.println("");
+  delay(50);
 }
 
 int smooth(int data, float smoothedVal){
